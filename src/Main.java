87424ba906f66fff1312aa52302a7dfa72260658
Hagith <hagithisshiah@gmail.com>
2023-01-07 21:54:36 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,56 @@ import Taxil_Booking.Taxi;
 
 import java.util.Scanner;
 
+import static java.sql.Types.NULL;
+
 public class Main
 {
     static Taxi[] t = new Taxi[100];
     static Customer[] c = new Customer[100];
     static Booking[] b = new Booking[100];
     static Scanner sc = new Scanner(System.in);
-    static  int i=0;
-    static int j=0;
-    static int k=0;
-    public static void taxi_creation()
+    static int taxi_id =0;//VARIABLES MUST BE MEANINGFULLY
+    static int cus_id =0;
+    static int book_id =0;
+    public static void taxi_creation(int taxi_id1)
     {
         System.out.println("Enter Driver's name: ");
-        t[i] = new Taxi(i+1,sc.next());
+        t[taxi_id1] = new Taxi(taxi_id1 +1,sc.next());
         System.out.println("Taxi was Created");
-        i++;
+        taxi_id++;
     }
-    public static void cus_creation()
+    public static void cus_creation(int cus_id1)
     {
         System.out.println("Enter your name: ");
-        c[j] = new Customer(j++,sc.next());
+        sc.nextLine();
+        String name = sc.nextLine();
+        c[cus_id1] = new Customer(cus_id1++,name);
+        cus_id++;
         System.out.println("Customer was created");
 
     }
-    public static void booking()
+    public static void booking(int taxi_id1,int book_id1)
     {
-        if(i>k)
+        if(taxi_id1 > book_id1)//REVERSE DIRECTION NEED TO BE HANDLED
+        {
+            System.out.println("Enter your ID: ");
+            int n = sc.nextInt();
+            System.out.println("Welcome " + c[n - 1].Customer_name);
+            System.out.println("Enter your pickup point");
+            char pick = sc.next().charAt(0);
+            System.out.println("Enter your dropping point");
+            char drop = sc.next().charAt(0);
+            System.out.println("Enter your pickup time");
+            int time = sc.nextInt();
+            int earn = ((((drop-pick)*15)-5)*10)+100;
+            t[book_id1].setEarnings(earn);
+            b[book_id1] = new Booking(book_id1++,pick,drop,time,time+(drop-pick),earn);
+            System.out.println("Taxi-"+ book_id1 + " booked");
+            t[book_id1 - 1].setCurrent_position(drop);
+            t[book_id1 - 1].setFree_time(time+(drop-pick));
+            book_id++;
+        }
+        else
         {
             System.out.println("Enter your ID: ");
             int n = sc.nextInt();
@@ -40,30 +64,17 @@ public class Main
             char drop = sc.next().charAt(0);
             System.out.println("Enter your pickup time");
             int time = sc.nextInt();
-            int earn = ((((drop-pick)*15)-5)*10)+100;
-            t[k].earnings = earn;
-            b[k] = new Booking(k++,pick,drop,time,time+(drop-pick),earn);
-            System.out.println("Taxi-"+k+ " booked");
-            t[k-1].current_position = drop;
-        }
-        else
-        {
-            System.out.println("Enter your ID: ");
-            int n = sc.nextInt();
-            System.out.println("Welcome " + c[n - 1].Customer_name);
-            System.out.println("Enter your pickup point");
-            char pick = sc.next().charAt(0);
             int count =0;
             char pos ='Z';
-            int e = 10000;
+            int e = NULL;//NULL
             int a = 0;
-            for (int l = 0; l < i; l++)
+            for (int l = 0; l < taxi_id1; l++)
             {
-                if(pick >= t[l].current_position)
+                if(time >= t[l].getFree_time())
                 {
-                    if(pick<pos)
+                    if(t[l].getCurrent_position()<pos)
                     {
-                        pos = pick;
+                        pos = t[l].getCurrent_position();
                     }
                 }
                 else
@@ -71,48 +82,49 @@ public class Main
                     count++;
                 }
             }
-            if(count == i)
+            if(count == taxi_id1)
             {
                 System.out.println("All Taxi's are busy");
             }
             else
             {
-                for (int l = 0; l < i; l++)
+                for (int l = 0; l < taxi_id1; l++)
                 {
-                    if(t[l].current_position == pos)
+                    if(t[l].getCurrent_position() == pos)
                     {
-                        if(e>t[l].earnings)
+                        if(e> t[l].getEarnings()||e==NULL)
                         {
-                            e = t[l].earnings;
+                            e = t[l].getEarnings();
                             a = l;
                         }
                     }
-                }
-                System.out.println("Enter your droping point");
-                char drop = sc.next().charAt(0);
-                System.out.println("Enter your pickup time");
-                int time = sc.nextInt();
+                }//TAXIES CAN BE COME BACKWARD
                 int earn = ((((drop-pick)*15)-5)*10)+100;
-                b[k] = new Booking(k++,pick,drop,time,time+(drop-pick),earn);
-                t[a].earnings += earn;
+                b[book_id1] = new Booking(book_id1++,pick,drop,time,time+(drop-pick),earn);
+                t[a].setEarnings(t[a].getEarnings() + earn);
                 System.out.println("Taxi-"+(a+1)+ " booked");
-                t[a].current_position = drop;
+                t[a].setCurrent_position(drop);
+                t[a].setFree_time(time+(drop-pick));
+                book_id++;
             }
         }
     }
-    public static void display_the_earnings()
+    public static void display_the_earnings(int taxi_id1)
     {
-        for (int l = 0; l < i; l++)
+        for (int l = 0; l < taxi_id1; l++)
         {
-            System.out.println("Taxi - "+t[l].Taxi_number+"\nDriver's name - "+t[l].Driver_name+"\nTotal Earnings - "+t[l].earnings);
+            System.out.println("Taxi - "+ t[l].getTaxi_number() +"\nDriver's name - "+ t[l].getDriver_name() +"\nTotal Earnings - "+ t[l].getEarnings());
+            System.out.println("---------------------------------------------------");
         }
     }
-    public static void booking_details()
+    public static void  booking_details(int book_id1)
     {
-        for (int l = 0; l < k; l++)
+        for (int l = 0; l < book_id1; l++)
         {
             System.out.println("Booking Id: "+b[l].id+"\nPick: "+b[l].pick_point+"\nDrop: "+b[l].drop_point+"\nPick_time: "+b[l].pick_time+"\nDrop_time: "+b[l].drop_time+"\nEarnings: "+b[l].money);
+            System.out.println("-------------------------------------------------------------");
         }
+
     }
     public static void main(String[] args)
     {
@@ -130,19 +142,20 @@ public class Main
             switch(n)
             {
                 case 1:
-                    taxi_creation();
+                    taxi_creation(taxi_id);
                     break;
                 case 2:
-                    cus_creation();
+                    cus_creation(cus_id);
                     break;
                 case 3:
-                    booking();
+                    booking(taxi_id,book_id);
                     break;
                 case 4:
-                    display_the_earnings();
+                    display_the_earnings(taxi_id);
                     break;
                 case 5:
-                    booking_details();
+                    booking_details(book_id);
+                    break;
                 case 6:
                     a=false;
                     break;
--- src/Taxil_Booking/Taxi.java
+++ src/Taxil_Booking/Taxi.java
@@ -2,14 +2,45 @@ package Taxil_Booking;
 
 public class Taxi
 {
-    public int Taxi_number;
-    public String Driver_name;
-    public char current_position='A';
-    public int earnings =0;
+    int Taxi_number;//getter and setter
+    String Driver_name;
+    char current_position='A';
+    int earnings =0;
+    int free_time =0;
 
     public Taxi(int Taxi_number,String Driver_name)
     {
-        this.Taxi_number = Taxi_number;
-        this.Driver_name = Driver_name;
+        this.setTaxi_number(Taxi_number);
+        this.setDriver_name(Driver_name);
+    }
+
+    public int getTaxi_number()
+    {
+        return Taxi_number;
+    }
+
+    public void setTaxi_number(int taxi_number)
+    {
+        Taxi_number = taxi_number;
+    }
+
+    public String getDriver_name() {return Driver_name;}
+
+    public void setDriver_name(String driver_name) {Driver_name = driver_name;}
+
+    public char getCurrent_position() {return current_position;}
+
+    public void setCurrent_position(char current_position) {this.current_position = current_position;}
+
+    public int getEarnings() {return earnings;}
+
+    public void setEarnings(int earnings) {this.earnings = earnings;}
+
+    public int getFree_time() {
+        return free_time;
+    }
+
+    public void setFree_time(int free_time) {
+        this.free_time = free_time;
     }
 }
